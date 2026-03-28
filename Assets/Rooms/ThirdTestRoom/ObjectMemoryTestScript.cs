e4d58baa3265cf8dd000ad2f3572eec067439b7c
nacho98760 <nacho987600@gmail.com>
2026-03-28 14:33:02 -0300

    Added basic functionality for objectMemoryTest

--- Assets/Rooms/ThirdTestRoom/ObjectMemoryTestScript.cs
+++ Assets/Rooms/ThirdTestRoom/ObjectMemoryTestScript.cs
@@ -1,6 +1,43 @@
+using System;
 using UnityEngine;
 
 public class ObjectMemoryTestScript : MonoBehaviour
 {
-    [SerializeField] private GameObject[] foamSlots;
+    [SerializeField] private GameObject[] slots;
+
+    [NonSerialized] public bool isPlayerAllowedToPlay;
+    [NonSerialized] public bool wasItTheFirstSlotObjectTouched = true;
+    [NonSerialized] public bool didPlayerPick2Objects = false;
+
+    public GameObject[] slotsPickedToPlay;
+
+    private bool isSwitching = false;
+
+    private void Start()
+    {
+        isPlayerAllowedToPlay = true;
+        slotsPickedToPlay = new GameObject[2];
+    }
+
+    public void SwitchObjects()
+    {
+        if (isSwitching)
+            return;
+        
+        isSwitching = true;
+
+        GameObject firstObject = slotsPickedToPlay[0].transform.GetChild(0).gameObject;
+        GameObject secondObject = slotsPickedToPlay[1].transform.GetChild(0).gameObject;
+
+        firstObject.transform.parent = slotsPickedToPlay[1].transform;
+        firstObject.transform.position = slotsPickedToPlay[1].transform.position;
+
+        secondObject.transform.parent = slotsPickedToPlay[0].transform;
+        secondObject.transform.position = slotsPickedToPlay[0].transform.position;
+
+        Array.Clear(slotsPickedToPlay, 0, slotsPickedToPlay.Length);
+
+        wasItTheFirstSlotObjectTouched = true;
+        isSwitching = false;
+    }
 }
--- Assets/Rooms/ThirdTestRoom/ObjectMemoryTestSlotsScript.cs
+++ Assets/Rooms/ThirdTestRoom/ObjectMemoryTestSlotsScript.cs
@@ -1,13 +1,9 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class ObjectMemoryTestSlotsScript : MonoBehaviour
 {
     [SerializeField] private ObjectMemoryTestScript objectMemoryTestScript;
-
-    private void Awake()
-    {
-        //---
-    }
 
     private void OnMouseEnter()
     {
@@ -17,5 +13,26 @@ public class ObjectMemoryTestSlotsScript : MonoBehaviour
     private void OnMouseExit()
     {
         print("BYe");
+    }
+
+    private void OnMouseDown()
+    {
+        if (objectMemoryTestScript.isPlayerAllowedToPlay)
+        {
+            if (objectMemoryTestScript.wasItTheFirstSlotObjectTouched)
+            {
+                objectMemoryTestScript.wasItTheFirstSlotObjectTouched = false;
+                objectMemoryTestScript.slotsPickedToPlay[0] = this.gameObject;
+            }
+            else
+            {
+                //If the second slot object picked is the same as the first one, we do nothing
+                if (this.gameObject != objectMemoryTestScript.slotsPickedToPlay[0])
+                {
+                    objectMemoryTestScript.slotsPickedToPlay[1] = this.gameObject;
+                    objectMemoryTestScript.SwitchObjects();
+                }
+            }
+        }
     }
 }
