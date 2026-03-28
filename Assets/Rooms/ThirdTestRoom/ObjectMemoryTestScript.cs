bbdd2d6d04e2e8df76ddaaaf0e94d094d8c68f60
nacho98760 <nacho987600@gmail.com>
2026-03-27 22:39:54 -0300

    Removed tray and just kept the table model with a new texture. (Slots will be on the table itself)

--- Assets/Rooms/ThirdTestRoom/ObjectMemoryTestScript.cs
+++ Assets/Rooms/ThirdTestRoom/ObjectMemoryTestScript.cs
@@ -3,13 +3,4 @@ using UnityEngine;
 public class ObjectMemoryTestScript : MonoBehaviour
 {
     [SerializeField] private GameObject[] foamSlots;
-
-    public Material defaultFloorFoamMaterial;
-    public Material floorFoamMaterialWhileSelected;
-
-    public Material defaultWallFoamMaterial;
-    public Material wallFoamMaterialWhileSelected;
-
-    public Material defaultUpperWallFoamMaterial;
-    public Material upperWallFoamMaterialWhileSelected;
 }
